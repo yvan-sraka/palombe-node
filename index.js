968cd5bfdb4372978002d1a8c71555f09b75b97a
Yvan Sraka <yvan@sraka.pw>
2018-10-16 11:25:55 +0200

    v0.2.0

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
 const mkfifo = require("mkfifo");
 
+const PREFIX = "/tmp/palombe/";
+
 exports.send = function(name, value) {
-    const path = `/tmp/palombe/${name}`;
-    fs.mkdirSync(`/tmp/palombe/`);
+    const path = `${PREFIX}${name}`;
+    fs.mkdirSync(`${PREFIX}`);
     mkfifo.mkfifoSync(path, 0o600);
     fs.appendFileSync(path, value);
 }
 
 exports.receive = function(name) {
-    const path = `/tmp/palombe/${name}`
+    const path = `${PREFIX}${name}`
     while (!fs.existsSync(path));
     const output = fs.readFileSync(path);
     fs.unlinkSync(path);
