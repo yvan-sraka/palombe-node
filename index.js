9baa28f8fdca22fc0bfa4bfe74adda650a0f0bc8
Yvan Sraka <yvan@sraka.pw>
2018-10-19 10:04:41 +0200

    v0.3.2

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const fs = require("fs");
-const mkfifo = require("mkfifo");
+const fs = require("fs")
+const mkfifo = require("mkfifo")
 
-__mkfifo = function(name) {
-    const prefix = "/tmp/palombe/";
-    const path = `${prefix}${name}`;
-    fs.mkdirSync(prefix);
-    mkfifo.mkfifoSync(path, 0o600);
-    return path;
+__mkfifo = name => {
+    const prefix = "/tmp/palombe/"
+    const path = `${prefix}${name}`
+    fs.mkdirSync(prefix)
+    mkfifo.mkfifoSync(path, 0o600)
+    return path
 }
 
-exports.send = function(name, value) {
-    const path = __mkfifo(name);
-    fs.appendFileSync(path, value);
+exports.send = (name, value) => {
+    const path = __mkfifo(name)
+    fs.appendFileSync(path, value)
 }
 
-exports.receive = function(name) {
-    const path = __mkfifo(name);
-    const output = fs.readFileSync(path);
-    fs.unlinkSync(path);
-    return output;
+exports.receive = name => {
+    const path = __mkfifo(name)
+    const output = fs.readFileSync(path)
+    fs.unlinkSync(path)
+    return output
 }
